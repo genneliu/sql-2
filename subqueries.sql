3188a4cc39c7e329afa5cead0b808046dd4bc36f
Genne Liu <genneliu@Gennes-MacBook-Pro.local>
2021-09-10 11:58:09 -0700

    almost done with subqueries

--- subqueries.sql
+++ subqueries.sql
@@ -6,3 +6,15 @@ SELECT t.name
 FROM track t
 WHERE track_id IN (SELECT track_id FROM playlist_track WHERE playlist_id IN (SELECT playlist_id FROM playlist WHERE name ='Music'));
 
+SELECT t.name 
+FROM track t
+WHERE track_id IN (SELECT track_id FROM playlist_track WHERE playlist_id IN (SELECT playlist_id FROM playlist WHERE playlist_id = 5));
+
+SELECT t.name
+FROM track t 
+WHERE genre_id IN (SELECT genre_id FROM genre WHERE name = 'Comedy');
+
+SELECT t.name
+FROM track t 
+WHERE album_id IN (SELECT album_id FROM album WHERE title = 'Fireball');
+
